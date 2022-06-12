9e70dfbb4ea35c7eabc3ebdc74d4e59b64bcfc6e
Henrique Santana <henricke.ss@gmail.com>
2022-06-12 20:16:13 -0300

    Include tests

---
+++ src/main/java/henriquesantos/com/pipeline/domain/TimeLocal.java
@@ -0,0 +1,5 @@
+package henriquesantos.com.pipeline.domain;
+
+public record TimeLocal(String dateTime, String zone) {
+    
+}
--- src/main/java/henriquesantos/com/pipeline/service/PipelineService.java
+++ src/main/java/henriquesantos/com/pipeline/service/PipelineService.java
@@ -1,11 +1,30 @@
 package henriquesantos.com.pipeline.service;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.stereotype.Service;
+
+import henriquesantos.com.pipeline.domain.TimeLocal;
 
 @Service
 public class PipelineService {
 
-    public String run(){
-        throw new IllegalArgumentException("Fail ArgumentException");
+    public String run() {
+        return "Hello World";
+    }
+
+    public TimeLocal dateTimeNow() {
+        return new TimeLocal(this.dateTimeFormat(), this.getZone());
+    }
+
+    public String dateTimeFormat(){
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        return LocalDateTime.now().format(dateTimeFormatter);
+    }
+
+    private String getZone(){
+        return ZoneId.of("America/Sao_Paulo").toString();
     }
 }
